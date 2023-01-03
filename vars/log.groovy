6749e7842fdb4fa2022f458b4cbe52dd6d2c74eb
Avinash Yadav <avinash.guruka@gmail.com>
2023-01-03 21:14:30 +0530

    updating roboshop-jenkins-shared-library

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,7 +1,30 @@
-def info(message) {
-    echo "INFO: ${message}"
-}
+//def info(message) {
+//    echo "INFO: ${message}"
+//}
+//
+//def warning(message) {
+//    echo "WARNING: ${message}"
+//}
 
-def warning(message) {
-    echo "WARNING: ${message}"
+def call(CASTE) {
+
+    pipeline {
+        agent any
+
+        stages {
+            stage('avinash') {
+                steps {
+                    echo "Guruka Avinash ${CASTE}"
+                }
+            }
+        }
+        stages {
+            stage('arun') {
+                steps {
+                    echo "Guruka Avinash ${CASTE}"
+                }
+            }
+        }
+    }
+
 }
