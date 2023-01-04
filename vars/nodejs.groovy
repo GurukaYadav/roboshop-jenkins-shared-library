54c261f74532a7f9fb8a9d27d5e80b128a211d92
Avinash Yadav <avinash.guruka@gmail.com>
2023-01-04 13:12:47 +0530

    updating roboshop-jenkins-shared-library

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,26 +1,26 @@
-pipeline {
-  agent any
-
-  stages {
-    stage('Download Dependencies') {
-      steps {
-        sh 'npm install'
-      }
-    }
-
-  }
-
-
-}
-
-//def call() {
-//  node() {
+//pipeline {
+//  agent any
+//
+//  stages {
 //    stage('Download Dependencies') {
-//      sh '''
-//        ls -ltr
-//        npm install
-//      '''
+//      steps {
+//        sh 'npm install'
+//      }
 //    }
+//
 //  }
 //
+//
 //}
+
+def call() {
+  node() {
+    stage('Download Dependencies') {
+      sh '''
+        ls -ltr
+        npm install
+      '''
+    }
+  }
+
+}
