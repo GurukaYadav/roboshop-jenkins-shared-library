e4e13fd71307e54688cb4833b7b4eed241eb10b6
Avinash Yadav <avinash.guruka@gmail.com>
2023-01-04 17:11:47 +0530

    updating roboshop-jenkins-shared-library

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,6 +1,6 @@
 def pipelineInit() {
   stage('Initiate Repo') {
     sh 'rm -rf *'
-    git branch: 'main', url: "https://github.com/GurukaYadav/${COMPONENT}.git\"
+    git branch: 'main', url: "https://github.com/GurukaYadav/${COMPONENT}.git"
   }
 }
