bd6540a402e8d1970804d228675801cadfa8169f
Avinash Yadav <avinash.guruka@gmail.com>
2023-02-25 13:04:56 +0530

    updating roboshop-jenkins-shared-library

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -9,8 +9,8 @@ def call() {
       stage('Terraform init') {
         sh 'terraform init'
       }
-      stage('Terraform plan -var-file=env/${ENV}.tfvars') {
-        sh 'terraform plan'
+      stage('Terraform plan') {
+        sh 'terraform plan -var-file=env/${ENV}.tfvars'
       }
       stage('Terraform apply') {
         sh 'terraform apply -auto-approve -var-file=env/${ENV}.tfvars'
