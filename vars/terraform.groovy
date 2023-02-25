73f761fa6a1c28652f52f9c36c0a203a121efaca
Avinash Yadav <avinash.guruka@gmail.com>
2023-02-25 12:28:21 +0530

    updating roboshop-jenkins-shared-library

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -1,19 +1,18 @@
 def call() {
   node() {
-    options {
-      ansiColor('xterm')
-    }
     parameters {
       choice(choices: ['dev', 'prod'], description: "Choose Environment", name: "ENV")
     }
-    stage('Terraform init') {
-      sh 'terraform init'
-    }
-    stage('Terraform plan') {
-      sh 'terraform plan'
-    }
-    stage('Terraform apply') {
-      sh 'terraform apply -auto-approve'
+    ansiColor('xterm') {
+      stage('Terraform init') {
+        sh 'terraform init'
+      }
+      stage('Terraform plan') {
+        sh 'terraform plan'
+      }
+      stage('Terraform apply') {
+        sh 'terraform apply -auto-approve'
+      }
     }
   }
 }
