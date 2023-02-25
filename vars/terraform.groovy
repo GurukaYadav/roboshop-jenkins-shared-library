c09ec2766b6cc6b70f188fb2df8202fe6f46d86d
Avinash Yadav <avinash.guruka@gmail.com>
2023-02-25 16:42:31 +0530

    updating roboshop-jenkins-shared-library

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -8,7 +8,8 @@ def call() {
     ansiColor('xterm') {
 //  Here the checkout(code clone) from git is automatically done in declarative pipelines but in scripted pipelines we need to specifically add a stage to clone the repo //
       stage('Code Checkout') {
-        sh 'rm -rf *'
+
+        sh 'find . | xargs rm -rf | sed -e "1d"'
         git branch: 'main', url:'https://github.com/GurukaYadav/terraform-roboshop-mutable.git'
       }
       stage('Terraform init') {
