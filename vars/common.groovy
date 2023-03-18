3e74d50cdd03717833003fb12a55285fd148a0f5
Avinash Yadav <avinash.guruka@gmail.com>
2023-03-18 22:03:01 +0530

    updating roboshop-jenkins-shared-library

--- vars/common.groovy
+++ vars/common.groovy
@@ -185,6 +185,8 @@ def publishAMI() {
     sh '''
       terraform init
       terraform apply -auto-approve  -var APP_VERSION=${TAG_NAME}
+      terraform state rm 'module.immutable-app-ami.aws_ami_from_instance.ami'
+      terraform destroy -auto-approve  -var APP_VERSION=${TAG_NAME}
     '''
   }
 }
